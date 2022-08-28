c47e2a62d8419bf91833134ef48f08b2052d0222
Lakshya Gupta <lakshyagupta26@live.com>
2022-08-28 21:06:47 +0530

    Initial Commit

---
+++ CastMac/CastMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastMacApp.swift
+//  CastMac
+//
+//  Created by Lakshya Gupta on 2022-08-28.
+//
+
+import SwiftUI
+
+@main
+struct CastMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CastMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CastMac
+//
+//  Created by Lakshya Gupta on 2022-08-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
